f118a3b407b706ad58d87e4d376cb17d722e5f42
happygeek21 <hemanthprabhu2003@gmail.com>
2023-08-18 13:07:35 +0530

    updated 18-08-23

---
+++ home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+    <form action="" method="post">
+        <div class="nav_links">
+            <div class="logo">
+                <a href="home.php"><img src="logo2.png"  alt="#"></a>
+                
+            </div>
+            
+            <ul>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a id=button href="indexreg.php">Sign Up</a></li>
+            
+            </ul>
+        </div>
+
+        <div class="support">
+            <p>Popular Brands We Support</p>
+        </div>
+        
+        <div class="brands">
+            <div class="images">
+                <img class="img1" src="samsung.png" alt="">
+                <img class="img2" src="apple.png" alt="">
+                <img class="img3" src="mi.png" alt="">
+                <img class="img4" src="oneplus.png" alt="">
+            </div>
+        </div>
+        
+    <div class="content">
+            
+            <h3>The Repair Process</h3>
+            <p>We Make Sure That Every Customer is satisfied with our Service.</p>
+            <p>We Provide Fast & Reliable Repair Charges.</p>
+            <p>Customers Responses Are Our First Priority.</p>
+    </div>
+
+    
+     
+        
+
+    </form>
+    
+
+    <footer>
+        <div class="footer-content">
+            <h3>The Phone Repair Shop</h3>
+        
+            <div class="para">
+                <p>Welcome To Phone Repair Shop, We Provide Best Service Possible to Our Customers.</p>
+            </div>
+            <div class="footer-imgs">
+                <ul>
+                    <li><a href="home.php"><img class="insta" src="instagram.svg"  alt="#"></a></li>
+                    <li><a href="home.php"><img class="fb" src="facebook.svg"  alt="#"></a></li>
+                    <li><a href="home.php"><img class="xlogo" src="xwhite.png"  alt="#"></a></li>
+            
+                </ul> 
+                
+                
+                
+            </div>
+            <div class="footer-imgs2">
+                <div class="contact-phone">
+                   
+                    <p>+91 9567297230</p>
+                </div>
+                
+            </div>
+            <div class="copyright">
+                <p>Copyright &copy; The Phone Repair Shop Designed By Hemanth & Arjun</p> 
+            </div>
+    
+        </div>
+    </footer>
+
+   
+</body>
+</html>
---
+++ input-details.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="input.css">
+</head>
+<body>
+
+    <form action="" method="post">
+        <div class="nav_links">
+            <div class="logo">
+                <a href="home.php"><img src="logo2.png"  alt="#"></a>
+                
+            </div>
+            
+            <ul>
+                <li><a href="#">How Does This Work?</a></li>
+                <li><a href="#">Any Queries Contact Us</a></li>
+                
+            
+            </ul>
+
+        </div>
+
+        <div class="support">
+            <p>Mention Your Queries Here</p>
+        </div>
+        
+        <div class="flex-container">
+            
+            <div class="input1">
+                
+            
+                <label for="">Select Your Phone Brand</label>
+                <select name="phones" id="phones">
+                        <option value="SAMSUNG">Samsung</option>
+                        <option value="APPLE">Apple</option>
+                        <option value="XIAOMI">Xiaomi</option>
+                        <option value="ONEPLUS">Oneplus</option>
+                    </select>
+            </div>       
+            <div class="input3">
+                <label for="">Date</label>
+                <input type="date" name="" id="">
+            </div>
+                  
+            
+               
+            <div class="input2">
+            <label for="">Mention Your Phone Complaint</label>
+                <select name="complaint" id="complaint">
+                    <option value="SAMSUNG">Display Complaint</option>
+                    <option value="APPLE">Dead Device</option>
+                    <option value="XIAOMI">Speaker Dead</option>
+                    <option value="ONEPLUS">Microphone Dead</option>
+                    
+                </select>
+            
+                
+            </div>    
+            <div class="input4">
+                <textarea name="" id="" cols="40" rows="6" placeholder="Description"></textarea>
+            </div>
+                
+            <div class="button">
+                <input type="submit" value="Submit">
+                <input type="reset" value="Cancel">
+            </div>    
+
+                
+            
+        </div>
+    </form> 
+        
+
+    
+    
+
+    
+
+   
+</body>
+</html>
